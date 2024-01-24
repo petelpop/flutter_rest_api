767cf7730a5cdbe383299bf327034b1a1f8c9c33
ipal <72238450+petelpop@users.noreply.github.com>
2024-01-24 08:55:40 +0700

    get api from reqres.in/api

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,3 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_rest_api/models/user.dart';
+import 'package:http/http.dart'as http;
 class UserService {
   static const String baseUrl = 'https://reqres.in/api';
+  Future <List<User>> fetchUsers() async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/users?page=2'));
+      final body = response.body;
+      final result = jsonDecode(body);
+      List<User> users = List<User>.from(
+        result['data'].map(
+          (user) => User.fromJson(user),
+        ),
+      );
+      return users;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 }
